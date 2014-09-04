ebe6e542ed7a9fa455b28f0dc87ed2b44cfb8c37
Matt Cook <matt@zebradog.com>
2014-09-04 16:58:20 -0500

    updated youtube functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ var width = 1920;
 var height = 1080;
 
 $(function(){
+
+   $('#nav .next').click(function(e){
+      e.preventDefault();
+      next();
+   });
+
+   $('#nav .prev').click(function(e){
+      e.preventDefault();
+      prev();
+   });
+
 	$( window ).resize(resize);
 	resize();
 });
@@ -9,6 +20,7 @@ $(function(){
 function resize(){
 	var w = $("#content").width()-1;
 	var h = $("#content").height()-1;
+
 	var scaleFactor = width/height < w/h ? w/width : h/height;
 
 	$('#demo').css({
@@ -19,3 +31,24 @@ function resize(){
 	});
 }
 
+function next(){
+  var $n = $('#nav li.demo.selected').next();
+  if(!$n.length) $n = $('#nav li.demo').first();
+  window.location =  $n.find('a').attr('href');
+}
+
+function prev(){
+  var $n = $('#nav li.demo.selected').prev();
+  if(!$n.length) $n = $('#nav li.demo').last();
+  window.location =  $n.find('a').attr('href');
+}
+
+//remaining time in seconds
+function updateTime(t){
+  if(t >= 0){
+    var m = Math.floor(t/60);
+    var s = Math.round(t - m * 60);
+    if(s < 10) s = '0'+s;
+    $('.timer').text(m+":"+s);
+  }
+}
---
+++ js/youtube.js
@@ -0,0 +1,48 @@
+var onYouTubeIframeAPIReady;
+
+$(function(){
+
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  onYouTubeIframeAPIReady = function () {
+    player = new YT.Player('demo', {
+      height: '1920',
+      width: '1080',
+      videoId: YOUTUBE_ID,
+      playerVars: {
+        enablejsapi: 1,
+        origin: '*',
+        autoplay: 1,
+        controls: 0,
+        modestbranding: 1,
+        rel: 0,
+        showinfo: 0,
+        color: 'white'
+      },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  function onPlayerReady(event) {
+    var p = event.target;
+    p.playVideo();
+    setInterval(function(){
+        updateTime(p.getDuration() - p.getCurrentTime());
+    },500);
+  }
+
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING)
+      player.setPlaybackQuality(player.getAvailableQualityLevels()[0]);
+    else if (event.data == YT.PlayerState.ENDED) 
+      next(); 
+  }
+
+});
